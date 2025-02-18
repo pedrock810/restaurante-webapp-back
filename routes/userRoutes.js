1906e372c02185fd8b83d3fb23db0bfb359492da
Pedro Rangel <pedroranpsp@gmail.com>
2025-02-18 19:09:05 +0000

    6 commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ const prisma = new PrismaClient();
 const SECRET = process.env.JWT_SECRET || "meusegredo"; // Pegando do .env
 
 // 🟢 Rota para criar um usuário
-roter.post("/register", async (req, res) => {
+router.post("/register", async (req, res) => {
   const { name, email, password, isAdmin } = req.body; // Adicionamos "isAdmin"
   const hashedPassword = await bcrypt.hash(password, 10);
 
