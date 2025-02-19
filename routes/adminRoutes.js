ad2e4dcabded587cf7a060cd0e8675775748943b
Pedro Rangel <pedroranpsp@gmail.com>
2025-02-19 18:09:25 +0000

    Criando rotas de administrador parte 1

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,7 +6,24 @@ const router = express.Router();
 // 🔹 Rota protegida - Somente administradores podem acessar
 router.get("/admin-dashboard", authenticate, isAdmin, (req, res) => {
     res.json({ message: "Bem-vindo ao painel de administração!" });
-  });
+});
   
+// 🔹 Rota para listar todos os usuários (Somente para Admins)
+router.get("/users", authenticate, isAdmin, async (req, res) => {
+  try {
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        isAdmin: true,
+      },
+    });
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar usuários" });
+  }
+});
+
 module.exports = router;
   
